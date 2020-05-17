16777db5b49ba5297f6853c30c2ce9367b6fcf9f
何军 <jun.he@chatin.com>
2020-05-17 19:05:26 +0800

    前端资源修改

---
+++ bird/app/controller/Index.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\controller;
+
+use app\BaseController;
+
+
+class Index
+{
+
+    public function index()
+    {   
+        return view('/webapp/index');
+    }
+
+    public function welcome()
+    {   
+        return view('/webapp/welcome');
+    }
+
+    public function staff_info_manage()
+    {
+        return view("/webapp/staff_info_manage");
+    }
+
+    public function staff_info_add()
+    {
+        return view("/webapp/staff_info_add");
+    }
+    
+}
