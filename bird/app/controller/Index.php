f15f0268d1dceb8ec949c67a60709541bd129778
shanliangLS <986747978@qq.com>
2020-05-18 13:35:57 +0800

    竞争对手列表、添加、删除、修改

--- bird/app/controller/Index.php
+++ bird/app/controller/Index.php
@@ -18,14 +18,24 @@ class Index
         return view('/webapp/welcome');
     }
 
-    public function staff_info_manage()
+    public function staff_manage()
     {
-        return view("/webapp/staff_info_manage");
+        return view("/webapp/staff/staff_manage");
     }
 
     public function staff_info()
     {
-        return view("/webapp/staff_info");
+        return view("/webapp/staff/staff_info");
+    }
+
+    public function competitor_manage()
+    {
+        return view("/webapp/competitor/competitor_manage");
+    }
+
+    public function competitor_info()
+    {
+        return view("/webapp/competitor/competitor_info");
     }
     
 }
